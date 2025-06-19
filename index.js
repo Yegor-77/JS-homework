48b754c3e84be277e041d4c71753a219b6c6fd4e
Yegor-77 <egorhmeluk77@gmail.com>
2025-06-19 19:33:01 +0200

    go

---
+++ index.js
@@ -0,0 +1,36 @@
+//1
+let age = 10;
+console.log(age);
+
+//2
+const name = "Yehor";
+console.log(name);
+
+//3
+const isStudent = true;
+console.log(isStudent);
+
+//4
+const myString = "I am a: Бархотние тяги";
+console.log(myString);
+
+//5
+let myNumber = 25;
+console.log(myNumber + 10);
+
+//6
+const myNull = null;
+console.log(myNull);
+
+//7
+const userName = prompt("Підскажіть нам ваше імя та првзіище");
+alert("поздровляю тепер вас звати:", userName);
+console.log(typeof userName);
+
+//8
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку");
+console.log(userAnswer);
+
+//9
+const uvaga = alert("Увага! Дія може бути небезпечною.");
+const uvagaA = confirm("Ви впевнені, що хочете продовжити?");
