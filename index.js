fd2d2240712d3d706e957b70cb052f22dafbb651
Yegor-77 <egorhmeluk77@gmail.com>
2025-06-30 18:14:25 +0200

    df

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 //1
-let age = 10;
-console.log(age);
+const celsius = 15;
+const fahrenheit = 59;
+console.log(fahrenheit);
 
-//2
-const name = "Yehor";
-console.log(name);
+//2 роблю це тяжкішим способом , але і так норм
+const daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log(daysInMonth);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
 
-//3
-const isStudent = true;
-console.log(isStudent);
+//3 game
+let health = 10;
+let energy = 100;
+console.log(health - 2);
+console.log(energy - 20);
 
 //4
-const myString = "I am a: Бархотние тяги";
-console.log(myString);
+let totalPrice = 100;
+console.log(totalPrice - 0.3); //ну крч
 
 //5
-let myNumber = 25;
-console.log(myNumber + 10);
+const floatNumber = 12.78;
+console.log(Math.floor(12.78));
 
 //6
-const myNull = null;
-console.log(myNull);
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
 
 //7
-const userName = prompt("Підскажіть нам ваше імя та првзіище");
-alert("поздровляю тепер вас звати:", userName);
-console.log(typeof userName);
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
 
 //8
-const userAnswer = confirm("Ви дійсно хочете покинути сторінку");
-console.log(userAnswer);
+const number = 12;
+const sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
 
 //9
-const uvaga = alert("Увага! Дія може бути небезпечною.");
-const uvagaA = confirm("Ви впевнені, що хочете продовжити?");
+const integer = 42;
+const stringNumber = "256";
+const convertedInt = stringNumber;
+console.log(convertedInt);
+
+const convertedString = convertedInt;
+console.log(convertedString);
