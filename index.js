3c35e56cc0934c4c08b262f5c702fb32070c343f
Yegor-77 <egorhmeluk77@gmail.com>
2025-07-05 20:45:15 +0200

    btr

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
 //1
-const celsius = 15;
-const fahrenheit = 59;
-console.log(fahrenheit);
+let result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
 
-//2 роблю це тяжкішим способом , але і так норм
-const daysInMonth = 30;
-let hoursInMonth = daysInMonth * 24;
-let minutesInMonth = hoursInMonth * 60;
-console.log(daysInMonth);
-console.log(hoursInMonth);
-console.log(minutesInMonth);
+//2
+let email = "egorhmeluk@gmail.com";
 
-//3 game
-let health = 10;
-let energy = 100;
-console.log(health - 2);
-console.log(energy - 20);
+let hasAt = email.includes("@");
+
+let length = email.length;
+
+console.log("Email містить '@':", hasAt);
+console.log("Кількість символів у email:", length);
+
+//3
+let wordOne = "My";
+let wordTwo = "name";
+let wordTHre = "is";
+
+let fullName = wordOne + wordTwo + wordTHre;
+
+fullName = fullName + "Yehor";
+
+console.log(fullName);
 
 //4
-let totalPrice = 100;
-console.log(totalPrice - 0.3); //ну крч
+let userNames = "imYehorkaa";
+let payments = 300;
 
-//5
-const floatNumber = 12.78;
-console.log(Math.floor(12.78));
-
-//6
-const floatString = "45.67";
-const parsedFloat = parseFloat(floatString);
-console.log(parsedFloat);
-
-//7
-const intString = "123";
-const parsedInt = parseInt(intString);
-console.log(parsedInt);
-
-//8
-const number = 12;
-const sqrtNumber = Math.sqrt(number);
-console.log(sqrtNumber);
-
-//9
-const integer = 42;
-const stringNumber = "256";
-const convertedInt = stringNumber;
-console.log(convertedInt);
-
-const convertedString = convertedInt;
-console.log(convertedString);
+alert(`Дякуємо, ${userNames}! До сплати ${payments} гривень`);
