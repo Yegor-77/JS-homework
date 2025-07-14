0bb947464ea6565e88d94ede3cbe417ec8dc092d
Yegor-77 <egorhmeluk77@gmail.com>
2025-07-14 18:03:19 +0200

    ertrytryj

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 //1
-let result = 5 + 5 + "5";
-console.log(result);
-console.log(typeof result);
+let field1 = "текст один";
+let field2 = "щось друге";
+
+if (field1.trim() !== "" && field2.trim() !== "") {
+  console.log(" 1 Обидва поля заповнені");
+} else {
+  console.log(" 1 Не всі поля заповнені");
+}
 
 //2
-let email = "egorhmeluk@gmail.com";
+let get1 = 4;
+let get2 = 4;
 
-let hasAt = email.includes("@");
-
-let length = email.length;
-
-console.log("Email містить '@':", hasAt);
-console.log("Кількість символів у email:", length);
+if (get1 + get2 >= 10) {
+  console.log(" 2 Сума менша або дорівнює 10");
+} else {
+  console.log(" 2 Сума менша або дорівнює 10");
+}
 
 //3
-let wordOne = "My";
-let wordTwo = "name";
-let wordTHre = "is";
+const roger = "javaReact";
 
-let fullName = wordOne + wordTwo + wordTHre;
-
-fullName = fullName + "Yehor";
-
-console.log(fullName);
+if (roger.includes("JavaScript")) {
+  console.log(" 3 Текст містить слово JavaScript");
+} else {
+  console.log(" 3 Текст не містить слово JavaScript");
+}
 
 //4
-let userNames = "imYehorkaa";
-let payments = 300;
+let число = 15;
 
-alert(`Дякуємо, ${userNames}! До сплати ${payments} гривень`);
+if (число > 10 && число < 20) {
+  console.log(" 4 Число входить в діапазон від 10 до 20");
+} else {
+  console.log(" 4 Число не входить в діапазон від 10 до 20");
+}
+
+//5
