29ad1b08b2cae24de069230fc8062240a43a1535
Yegor-77 <egorhmeluk77@gmail.com>
2025-08-11 18:14:41 +0200

    ewffadds

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-//1
-let carentClients = 0;
-const maxClient = 10;
+// 1
+let numbers1 = [1, 2, 3];
+numbers1[1] = 10;
+console.log("1:", numbers1);
 
-while (carentClients < maxClient) {
-  carentClients += 1;
-  console.log("кількість", carentClients);
-  if (carentClients === 10) {
-    console.log("все");
+// 2
+let strings1 = ["яблуко", "банан", "груша"];
+strings1.push("апельсин");
+console.log("2:", strings1);
+
+// 3
+let numbers2 = [4, 7, 2, 9];
+let sum = 0;
+for (let i = 0; i < numbers2.length; i++) {
+  sum += numbers2[i];
+}
+console.log("3: Сума =", sum);
+
+// 4
+let numbers3 = [5, 10, 15, 20, 25];
+console.log("4:");
+for (let i = 0; i < numbers3.length; i++) {
+  console.log(numbers3[i]);
+}
+
+// 5
+let strings2 = ["сонце", "програмування", "кіт", "машина", "телефон"];
+console.log("5:");
+for (let i = 0; i < strings2.length; i++) {
+  if (strings2[i].length > 5) {
+    console.log(strings2[i]);
   }
 }
 
-//2
-let i;
-
-for (let i = 2; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
+// 6
+let numbers4 = [3, 8, 12, 5, 22, 9, 1, 30, 15, 18];
+let max = numbers4[0];
+for (let i = 1; i < numbers4.length; i++) {
+  if (numbers4[i] > max) {
+    max = numbers4[i];
   }
 }
+console.log("6: Максимальне =", max);
 
-//3
-for (let b = 1; b <= 10; b++) {
-  console.log(`7 x ${b} = ${7 * b}`);
-}
-
-//4
-let n = 5;
-
-for (let aaa = 0; aaa < 100; aaa++) {
-  if (aaa >= n) {
-    break;
+// 7
+let numbers5 = [4, 7, 10, 13, 16, 21, 24, 28, 33, 40];
+console.log("7: Парні числа:");
+for (let i = 0; i < numbers5.length; i++) {
+  if (numbers5[i] % 2 === 0) {
+    console.log(numbers5[i]);
   }
-  console.log(aaa);
-}
-
-//5
-let g = 1;
-
-while (g <= 20) {
-  if (g % 3 === 0) {
-    g++;
-    continue;
-  }
-  console.log(g);
-  g++;
 }
