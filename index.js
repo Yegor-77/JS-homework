8dbc2aa668d03050f32b390ca85eddda34f77bf4
Yegor-77 <egorhmeluk77@gmail.com>
2025-09-24 19:31:12 +0200

    6543

--- index.js
+++ index.js
@@ -1,53 +1,81 @@
-//
+//1
 
-const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
-const words = ["кіт", "собака", "миша", "папуга", "хомяк"];
+let bankAccount = {
+  ownerName: "Єгор Хмелюк",
+  accountNumber: "UA12345678765",
+  balance: 0,
 
-function countItems(array, condition) {
-  let count = 0;
-  for (const element of array) {
-    if (condition(element)) {
-      count++;
+  deposit: function (amount) {
+    this.balance += amount;
+    alert(
+      `Ви поповнили свій щет на ${amount} гривнів Залишок: ${this.balance} гривнів`
+    );
+  },
+
+  withdraw: function (amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+      alert(`Ви зняли ${amount} гривнів Залишок: ${this.balance} гривнів`);
+    } else {
+      alert("Недостатньо гривнів на рахунку!");
     }
-  }
-  return count;
+  },
+};
+
+if (confirm("Хочете поповнити рахунок?")) {
+  let sum = Number(prompt("Введіть суму для поповнення:"));
+  bankAccount.deposit(sum);
+} else if (confirm("Хочете зняти готівку?")) {
+  let sum = Number(prompt("Введіть суму для зняття:"));
+  bankAccount.withdraw(sum);
 }
 
-const isEven = (num) => num % 2 === 0;
-const isLarge = (num) => num > 10;
-const isShort = (word) => word.length <= 3;
+// 2
+let weather = {
+  temperature: Number(prompt("Введіть температуру (°C):")),
+  humidity: 60,
+  windSpeed: 15,
 
-console.log("Парних чисел:", countItems(numbers, isEven));
-console.log("Чисел більше 10:", countItems(numbers, isLarge));
-console.log("Коротких слів:", countItems(words, isShort));
+  isFreezing: function () {
+    return this.temperature < 0;
+  },
+};
 
-// 2 це я не дуже розумію як робити
+if (weather.isFreezing()) {
+  alert("Температура нижче 0 градусів Цельсія");
+} else {
+  alert("Температура вище або рівна 0 градусів Цельсія");
+}
 
-//3
+// 3
+let user = {
+  name: "Олег",
+  email: "oleg@example.com",
+  password: "12345",
 
-function repeatMessage(times, messageCreator) {
-  for (let i = 0; i < times; i++) {
-    if (condition(element)) {
-      messageCreator(i);
+  login: function (inputEmail, inputPassword) {
+    if (this.email === inputEmail && this.password === inputPassword) {
+      alert("Вхід успішний!");
+    } else {
+      alert("Неправильний e-mail або пароль!");
     }
-  }
-  return count;
-}
+  },
+};
 
-//4
+let inputEmail = prompt("Введіть email:");
+let inputPassword = prompt("Введіть пароль:");
+user.login(inputEmail, inputPassword);
 
-function processMovies(movies, action) {
-  for (let i = 0; i < movies.length; i++) {
-    action(movies[i], i);
-  }
-}
+// 4
+let movie = {
+  title: "Інтерстеллар",
+  director: "Крістофер Нолан",
+  year: 2014,
+  rating: 8.6,
 
-function printMovie(movie, index) {
-  console.log(`${index + 1}. ${movie}`);
-}
+  isHighRated: function () {
+    return this.rating > 8;
+  },
+};
 
-const movies = ["Inception", "Interstellar", "Matrix", "Avatar"];
-
-console.log();
-processMovies(movies, printMovie);
-//кажетца так це має бути
+console.log(); //не знаю про кінець
